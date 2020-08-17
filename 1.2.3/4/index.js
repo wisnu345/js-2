2f49acfc0e7918747103f55cf84bb864b992946b
Cethol-zzz <wisnugiri64@gmail.com>
2020-08-17 22:20:17 +0700

    update fetch

--- 1.2.3/4/index.js
+++ 1.2.3/4/index.js
@@ -1,10 +1,28 @@
 const fetch = require('node-fetch');
+const express = require('express')
+const app = express()
+const port = 3000
+// let url = 'https://jsonplaceholder.typicode.com/users'
+// let set = {method: "Get"}
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(result => result.json())
-.then(json => {
-    json.map(e=>{
-    let {name}=e
-    console.log(name)
+app.get('/', (req, res) => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(result => result.json())
+        .then(resp => {
+            res.send(resp.map(e => e.name))
     })
 })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+
+// .then(result => result.json())
+// .then(json => {
+//     json.map(e=>{
+//     let {name}=e
+//     console.log(name)
+//     })
+// })
